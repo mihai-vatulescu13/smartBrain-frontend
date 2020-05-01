d2d6183e2129b991f15aef4ef62fb4db816bce67
sam smith <56478615+mihai-vatulescu13@users.noreply.github.com>
2020-05-01 14:20:44 +0300

    Updates

---
+++ precache-manifest.e6396b4431e6ce69e3b8d28fdac29eee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55573f8d3856ed5309513c6124186a05",
+    "url": "/mihai-vatulescu13/smartBrain-frontend/index.html"
+  },
+  {
+    "revision": "2a5ebf2b5c1bbeabc8cd",
+    "url": "/mihai-vatulescu13/smartBrain-frontend/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "7a128a2e14a00f654fc9",
+    "url": "/mihai-vatulescu13/smartBrain-frontend/static/css/main.b94f03f7.chunk.css"
+  },
+  {
+    "revision": "2a5ebf2b5c1bbeabc8cd",
+    "url": "/mihai-vatulescu13/smartBrain-frontend/static/js/2.acac7fda.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mihai-vatulescu13/smartBrain-frontend/static/js/2.acac7fda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a128a2e14a00f654fc9",
+    "url": "/mihai-vatulescu13/smartBrain-frontend/static/js/main.ab694caf.chunk.js"
+  },
+  {
+    "revision": "379fcfd178466f7ca95e",
+    "url": "/mihai-vatulescu13/smartBrain-frontend/static/js/runtime-main.f7f3ab2f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/mihai-vatulescu13/smartBrain-frontend/precache-manifest.e6396b4431e6ce69e3b8d28fdac29eee.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mihai-vatulescu13/smartBrain-frontend/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
